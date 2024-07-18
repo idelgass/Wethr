6b3ea4567f2161841f3945a8a12688e737304855
isaacdelgass <isaac@delgass.com>
2024-07-18 17:58:11 -0400

    Initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# wethr_app
+
+A minimalist weather app made with Flutter
---
+++ android/app/src/main/kotlin/com/idelgass/wethr_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.idelgass.wethr_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
