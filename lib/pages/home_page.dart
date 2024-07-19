5747bab97b099942a8ea601863066aa9a1324a45
isaacdelgass <isaac@delgass.com>
2024-07-19 13:56:45 -0400

    OpenWeatherMap connection

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather/weather.dart';
+import 'package:wethr_app/api_keys.dart';
 
 class HomePage extends StatefulWidget{
   const HomePage({super.key});
@@ -8,8 +10,48 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage>{
+
+  // TODO: Replace this with dynamic city selection using a separate page/dropdown
+  final String _cityName = "Philadelphia";
+
+  // Access weather for a specific location from OpenWeatherMap
+  final WeatherFactory _weatherFactory = WeatherFactory(OPEN_WEATHER_MAP_KEY);
+
+  // Stores weather-query response from OpenWeatherMap
+  Weather? _weather;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _weatherFactory.currentWeatherByCityName(_cityName).then((fweather){
+      // Ensure UI updates on retrieval
+      setState(() {
+        _weather = fweather;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext buildContext){
-    return Scaffold();
+    return Scaffold(body: _buildUI());
+  }
+
+  Widget _buildUI(){
+    // Loading screen
+    if(_weather == null){
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    // Load of OpenWeatherMap API successful
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      height: MediaQuery.sizeOf(context).height,
+      child: Column(
+        children: [],
+      )
+    );
   }
 }
