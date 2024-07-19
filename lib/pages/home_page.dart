0f75bda34d47c7e001ea04256f34e9e04736fd8e
isaacdelgass <isaac@delgass.com>
2024-07-19 14:45:25 -0400

    Display date and time

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:weather/weather.dart';
 import 'package:wethr_app/api_keys.dart';
 
@@ -37,9 +38,9 @@ class _HomePageState extends State<HomePage>{
     return Scaffold(body: _buildUI());
   }
 
-  Widget _buildUI(){
+  Widget _buildUI() {
     // Loading screen
-    if(_weather == null){
+    if (_weather == null) {
       return const Center(
         child: CircularProgressIndicator(),
       );
@@ -47,11 +48,74 @@ class _HomePageState extends State<HomePage>{
 
     // Load of OpenWeatherMap API successful
     return SizedBox(
-      width: MediaQuery.sizeOf(context).width,
-      height: MediaQuery.sizeOf(context).height,
-      child: Column(
-        children: [],
+        width: MediaQuery
+            .sizeOf(context)
+            .width,
+        height: MediaQuery
+            .sizeOf(context)
+            .height,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _displayLocation(),
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.05,
+            ),
+            _displayDateTime(),
+          ],
+        )
+    );
+  }
+
+  Widget _displayLocation(){
+    return Text(
+      _weather?.areaName ?? "",
+      style: const TextStyle(
+        //TODO: Set text style later
       )
     );
   }
+
+  Widget _displayDateTime(){
+    DateTime currentDateTime = _weather!.date!;
+    return Column(
+      children: [
+        // Display time as Hour:minutes am/pm
+        Text(
+          DateFormat("h:mm a").format(currentDateTime),
+          style: const TextStyle(
+            //TODO: Set text style later
+          )
+        ),
+        // Padding
+        const SizedBox(
+          height: 8,
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              // EEEE -> Day of the week
+              DateFormat("EEEE").format(currentDateTime),
+              style: const TextStyle(
+                //TODO: Set text style later
+              )
+            ),
+            Text(
+              // day, month, year
+              "  ${DateFormat("d.m.y").format(currentDateTime)}",
+              style: const TextStyle(
+                //TODO: Set text style later
+              )
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
 }
